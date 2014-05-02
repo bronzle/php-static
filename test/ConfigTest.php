333c60db70a1437a9f3ab6b31868e29c74b288c7
Byron Bischoff <byronb@gmail.com>
2014-05-02 13:53:24 -0700

    config test

---
+++ test/ConfigTest.php
@@ -0,0 +1,18 @@
+<?php
+class ConfigTest extends PHPUnit_Framework_TestCase {
+  public function setUp() {
+    $_SERVER['REQUEST_URI'] = '/';
+    $_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/data';
+    set_env('phps_app_doc_root', $_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/data');
+    run();
+  }
+  public function testConfigRead() {
+    $this->assertNotEmpty(config());
+  }
+  public function testConfigDefault() {
+    $this->assertNotEmpty(config('pages_root'));
+  }
+  public function testConfigJson() {
+    $this->assertEquals('value', config('test_param'));
+  }
+}
