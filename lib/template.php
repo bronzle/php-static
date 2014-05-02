3c00d57b4a80eab3e1706739afe379993876a929
Byron Bischoff <byronb@gmail.com>
2014-05-02 12:58:08 -0700

    add ability to call render from controller without setting it to content

--- lib/template.php
+++ lib/template.php
@@ -3,6 +3,7 @@
 function locate_template($template_location, $template, $allow_expanding_to_folder = true, $partial = true, &$files = array()) {
   list($template, $ext) = array_merge(explode('.', $template, 2), array('php'));
   $ext = ".{$ext}";
+  $files = array();
   if ($template_location[0] !== '/') {
     $template_location = request('root_path') . '/' . $template_location;
   }
@@ -33,17 +34,19 @@ function locate_template($template_location, $template, $allow_expanding_to_fold
   }
   return null;
 }
-function &render($template, $__variables = array(), $__partial = true, $__is_layout = null) {
+function &render($template, $__variables = array(), $__template_root = null, $__partial = true, $__is_layout = null, $__save_content = true) {
   $__internal = is_bool($__is_layout);
-  $__template = locate_template(config('pages_root'), $template, !$__is_layout, $__partial, $__should_be_at);
+  $__in_controller = isset($GLOBALS['__in_controller']) && $GLOBALS['__in_controller'] == true;
+  $__template = locate_template($__template_root ? $__template_root : config('pages_root'), $template, !$__is_layout, $__in_controller ? false : $__partial, $__should_be_at);
   if (!$__template) {
     throw new MissingTemplate(($__internal ? ($__is_layout ? MissingTemplate::LAYOUT : MissingTemplate::CONTENT) :  MissingTemplate::OTHER), $template, $__should_be_at);
   }
-  unset($__should_be_at);
-  unset($template);
-  unset($__is_layout);
-  unset($__internal);
-  return get_template_contents($__template, $__variables);
+  unset($__should_be_at, $template, $__is_layout, $__internal);
+  $contents = &get_template_contents($__template, $__variables);
+  if ($__in_controller && $__save_content) {
+    $GLOBALS['__content'] = &$contents;
+  }
+  return $contents;
 }
 function &get_template_contents($__template, $__variables = array()) {
   extract($__variables);
@@ -56,7 +59,7 @@ function &title($title = null) {
   static $__title = null;
   if (!$__title && $title) {
     $__title = $title;
-  } elseif (!$title) {
+  } elseif ($title === false) {
     $__title = '';
   }
   return $__title;
