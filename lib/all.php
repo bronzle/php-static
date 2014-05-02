508ca33f0c7128da4468905a5c1abb9f1e80d53a
Byron Bischoff <byronb@gmail.com>
2014-05-02 13:07:40 -0700

    config depends on request

--- lib/all.php
+++ lib/all.php
@@ -1,8 +1,8 @@
 <?php
 require(__DIR__ . '/helpers.php');
 require(__DIR__ . '/exceptions.php');
-require(__DIR__ . '/config.php');
 require(__DIR__ . '/request.php');
+require(__DIR__ . '/config.php');
 require(__DIR__ . '/template.php');
 require(__DIR__ . '/response.php');
 require(__DIR__ . '/optional/url.php');
